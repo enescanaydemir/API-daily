8c2a2b012c7f07b29b25dfd6236c6d769e82ee60
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2025-01-11 00:16:15 +0300

    ProductController added

--- ApiDaily.Api/Controllers/ProductController.cs
+++ ApiDaily.Api/Controllers/ProductController.cs
@@ -1,3 +1,5 @@
+using ApiDaily.Data.Abstract;
+using ApiDaily.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +9,25 @@ namespace ApiDaily.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        
+        private readonly IGenericRepository<Product> _genericRepository;
+
+        public ProductController(IGenericRepository<Product> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _genericRepository.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            var newProduct = await _genericRepository.AddAsync(product);
+            return Ok(newProduct);
+        }
     }
 }
